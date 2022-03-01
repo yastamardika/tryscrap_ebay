9ed5e987a130ba0d3f46e5caab5216a442109461
yastamardika <rattyasta1@gmail.com>
2022-03-02 01:46:34 +0700

    modify parser and seeder

---
+++ parsers/search.rb
@@ -0,0 +1,16 @@
+html = Nokogiri.HTML(content)
+
+products = html.css('.srp-results .s-item')
+
+products.each do |product|
+    name = product.css('.s-item__title').text
+    url = product.css('.s-item__link').attr('href').text
+
+    pages << {
+        url: url,
+        page_type: 'product',
+        vars: {
+            'name' => name,
+        }
+    }
+end
