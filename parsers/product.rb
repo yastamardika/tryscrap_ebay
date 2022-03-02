08ac92efb566ae429af2ae8d9e90c534f724769e
yastamardika <rattyasta1@gmail.com>
2022-03-02 23:25:07 +0700

    fix html identifier

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,5 +1,7 @@
 html = Nokogiri.HTML(content)
 price = html.css('#prcIsum').text 
+price = html.css('[itemprop="price"]').text if price.strip == '' 
+price = html.css('#prcIsum_bidPrice').text if price.strip == '' 
 raise 'something happend!' if price.strip == ''
 
 outputs <<{
